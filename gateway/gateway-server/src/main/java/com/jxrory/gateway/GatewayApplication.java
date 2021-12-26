4b418db79bfaba9dca9a2651e4b797ca8c4d8f93
Rory <jxrory@jxrory.com>
2021-12-26 14:33:26 +0800

    jxrory cloud gateway

---
+++ gateway/gateway-server/src/main/java/com/jxrory/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.jxrory.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author jxrory
+ * @date 2021/12/26 下午1:57
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
